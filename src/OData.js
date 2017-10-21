b06aa3af62fda1a78a63b8adc996a6f8d4ea45b3
Sean Lynch <techniq35@gmail.com>
2017-10-20 22:39:50 -0400

    Pass updateOptions down to react-fetch-component

--- src/OData.js
+++ src/OData.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import Fetch, { renderChildren } from 'react-fetch-component';
 import buildQuery from 'odata-query';
 
-function buildUrl (baseUrl, query) {
-  return (query !== false) && baseUrl + buildQuery(query)
+function buildUrl(baseUrl, query) {
+  return query !== false && baseUrl + buildQuery(query);
 }
 
 class OData extends Component {
   render() {
     const { baseUrl, query, children, ...rest } = this.props;
-    const url = buildUrl(baseUrl, query)
+    const url = buildUrl(baseUrl, query);
 
     return (
       <Fetch url={url} {...rest}>
         {({ fetch, ...props }) => {
           return renderChildren(children, {
             ...props,
-            fetch: (query, options) => fetch(buildUrl(baseUrl, query || this.props.query), options || this.props.options)
-          })
+            fetch: (query, options, updateOptions) =>
+              fetch(
+                buildUrl(baseUrl, query || this.props.query),
+                options || this.props.options,
+                updateOptions
+              )
+          });
         }}
       </Fetch>
-    )
+    );
   }
 }
 
