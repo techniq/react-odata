444667c805046914556b54a9c811309863baf5ad
c-harris <c.harris@communicationinnovations.com.au>
2020-05-13 19:21:50 +1000

    add isempty helper

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,9 @@
 export const isFunction = value => typeof value === 'function';
 export const isObject = value => typeof value === 'object';
+export const isEmpty = obj => {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
